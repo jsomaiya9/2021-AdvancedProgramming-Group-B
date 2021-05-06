c5651ed0542597a152271f0b22a1379a5bed91f9
Bilkis Islam <365405@my.leicestercollege.ac.uk>
2021-05-06 10:09:10 +0100

    Removed advert and updated database via migration

---
+++ BARDSports/BARDSports/Migrations/202105060905493_removedadvert.cs
@@ -0,0 +1,30 @@
+﻿namespace BARDSports.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changesrecommended : DbMigration
+    {
+        public override void Up()
+        {
+          
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.AdvertModels",
+                c => new
+                    {
+                        AdvertId = c.Int(nullable: false, identity: true),
+                        LeagueId = c.Int(nullable: false),
+                        Description = c.String(),
+                        Image = c.String(),
+                    })
+                .PrimaryKey(t => t.AdvertId);
+            
+            CreateIndex("dbo.AdvertModels", "LeagueId");
+            AddForeignKey("dbo.AdvertModels", "LeagueId", "dbo.LeagueModels", "LeagueId", cascadeDelete: true);
+        }
+    }
+}
